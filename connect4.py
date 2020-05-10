24a135f57e00de7e13a50d2a4cd11e03e6a57cd1
fredlc <fredlefevre@outlook.com>
2020-05-10 19:34:21 -0400

    Implemented functionality to check winning locations

--- connect4.py
+++ connect4.py
@@ -11,7 +11,7 @@ def drop_piece(board, row, col, piece):
 	board[row][col] = piece
 
 def is_valid_location(board, col):
-	return board[5][col] == 0
+	return board[ROW_COUNT - 1][col] == 0
 
 def get_next_open_row(board, col):
 	for r in range(ROW_COUNT):
@@ -20,6 +20,32 @@ def get_next_open_row(board, col):
 
 def print_board(board):
 	print(np.flip(board, 0))
+
+def winning_move(board, piece):
+	# Check horizontal locations for win
+	for c in range(COLUMN_COUNT - 3):
+		for r in range(ROW_COUNT):
+			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+				return True
+
+	# Check vertical locations for win
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT - 3):
+			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+				return True
+
+	# Check for positively sloped diagonals
+	for c in range(COLUMN_COUNT - 3):
+		for r in range(ROW_COUNT - 3):
+			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+				return True
+
+	# Check for negatively sloped diagonals
+	for c in range(COLUMN_COUNT - 3):
+		for r in range(3, ROW_COUNT):
+			if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+				return True
+
 
 board = create_board()
 print(board)
@@ -33,11 +59,19 @@ while not game_over:
 			row = get_next_open_row(board, col)
 			drop_piece(board, row, col, 1)
 
+			if winning_move(board, 1):
+				print("PLAYER 1 WINS!!!")
+				game_over = True
+
 	else:
 		col = int(input("Player 2, make your selection (0-6):"))
 		if is_valid_location(board, col):
 			row = get_next_open_row(board, col)
 			drop_piece(board, row, col, 2)
+
+			if winning_move(board, 2):
+				print("PLAYER 2 WINS!!!")
+				game_over = True
 
 	print_board(board)
 
